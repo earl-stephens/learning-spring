3cbb4483bf5d6a8dc444124e1a2ab5824359efef
earl-stephens <stephensearl@hotmail.com>
2023-05-08 20:25:50 -0600

    Edit AppStartupEvent to load DB

--- src/main/java/com/example/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/example/learningspring/util/AppStartupEvent.java
@@ -4,6 +4,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import com.example.learningspring.data.Guest;
+import com.example.learningspring.data.GuestRepository;
+import com.example.learningspring.data.Reservation;
+import com.example.learningspring.data.ReservationRepository;
 import com.example.learningspring.data.Room;
 import com.example.learningspring.data.RoomRepository;
 
@@ -13,15 +17,25 @@ public class AppStartupEvent implements ApplicationListener<ApplicationReadyEven
 	//before querying the db
 	
 	private final RoomRepository roomRepository;
+	private final GuestRepository guestRepository;
+	private final ReservationRepository reservationRepository;
 	
-	public AppStartupEvent(RoomRepository roomRepository) {
+	public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
 		this.roomRepository = roomRepository;
+		this.guestRepository = guestRepository;
+		this.reservationRepository = reservationRepository;
 	}
 	
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		Iterable<Room> rooms = this.roomRepository.findAll();
 		rooms.forEach(System.out::println);
+		
+		Iterable<Guest> guests = this.guestRepository.findAll();
+		guests.forEach(System.out::println);
+		
+		Iterable<Reservation> reservations = this.reservationRepository.findAll();
+		reservations.forEach(System.out::println);
 	}
 
 }
