5f69947e435ce08cda8b7137d2ebf439edd4003d
earl-stephens <stephensearl@hotmail.com>
2023-05-11 21:46:32 -0600

    My work on completing challenge

--- src/main/java/com/example/learningspring/webservice/WebserviceController.java
+++ src/main/java/com/example/learningspring/webservice/WebserviceController.java
@@ -3,9 +3,13 @@ package com.example.learningspring.webservice;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.learningspring.business.GuestService;
@@ -51,5 +55,11 @@ public class WebserviceController {
 	@RequestMapping(path="/rooms", method=RequestMethod.GET)
 	public List<Room> getRooms() {
 		return roomService.getRooms();
+	}
+	
+	@PostMapping("/guests")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addGuest(@RequestBody Guest guest) {
+		guestService.addGuest(guest);
 	}
 }
